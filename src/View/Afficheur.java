0232bffcd64152688c9750878c67519251c5a185
MaximeDito <dm106622@etu.unice.fr>
2014-12-23 15:21:43 +0100

    Correction mineure affichage. Compléxité de l'algorithme : O(n)

--- src/View/Afficheur.java
+++ src/View/Afficheur.java
@@ -51,7 +51,7 @@ public final class Afficheur {
         try {
             String head = "<html>"
                     + "\n<head>"
-                    + "\n<title>Statistique</title>"
+                    + "\n<title>R&eacute;sultat de la simulation | Marongiu et Dito</title>"
                     + "<link rel=\"stylesheet\" href=\"https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css\">"
                     + "<link rel=\"stylesheet\" href=\"https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap-theme.min.css\">"
                     + "<script src=\"https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js\"></script>"
